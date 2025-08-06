5fc0873a9b420645f1630d64ed57d0994c45ccd3
Antoni Zarzycki <145159295+AntoniZarzycki@users.noreply.github.com>
2025-08-06 16:57:23 +0200

    feat: add a click eventListener to all of the buttons

--- script.js
+++ script.js
@@ -32,3 +32,111 @@ function operate(operator, num1, num2) {
       return "Incorrect operator";
   }
 }
+
+document.addEventListener("click", (element) => {
+  if (!element.target.closest("button")) return;
+
+  const numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  const operators = ["add", "subtract", "multiply", "divide"];
+
+  const id = element.target.closest("button").id;
+
+  if (numbers.includes(id)) {
+    numberPressed(convertToNumber(id));
+  } else if (operators.includes(id)) {
+    operatorPressed(id);
+  } else {
+    switch (id) {
+      case "AC":
+        clearAll();
+        break;
+      case "C":
+        clear();
+        break;
+      case "backspace":
+        backspacePressed();
+        break;
+      case "brackets":
+        bracketsPressed();
+        break;
+      case "point":
+        pointPressed();
+        break;
+      case "equal":
+        equalPressed();
+        break;
+    }
+  }
+});
+
+function convertToNumber(string) {
+  switch (string.toLowerCase()) {
+    case "zero":
+      return 0;
+    case "one":
+      return 1;
+    case "two":
+      return 2;
+    case "three":
+      return 3;
+    case "four":
+      return 4;
+    case "five":
+      return 5;
+    case "six":
+      return 6;
+    case "seven":
+      return 7;
+    case "eight":
+      return 8;
+    case "nine":
+      return 9;
+    default:
+      return NaN;
+  }
+}
+
+function numberPressed(number) {
+  if (number === NaN) return;
+  console.log(number);
+}
+
+function operatorPressed(id) {
+  console.log(id);
+}
+
+function clearAll() {
+  console.log("Clear All");
+}
+
+function clear() {
+  console.log("Clear");
+}
+
+function backspacePressed() {
+  console.log("Backspace");
+}
+
+function bracketsPressed() {
+  console.log("Brackets");
+}
+
+function pointPressed() {
+  console.log("Point");
+}
+
+function equalPressed() {
+  console.log("Equal");
+}
